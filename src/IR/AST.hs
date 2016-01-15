6173488d627c815272d767cb87c9083b45be16b3
Nadrieril <nadrieril@gmail.com>
2016-01-15 14:51:51 +0100

    Pretty-print expressions

--- src/IR/AST.hs
+++ src/IR/AST.hs
@@ -1,7 +1,16 @@
 module IR.AST where
+import Text.Printf (printf)
 
 data BinOp = Multiply | Divide | Add | Subtract | Or | And
-  deriving (Show)
+
+instance Show BinOp where
+  show Multiply = "*"
+  show Divide = "/"
+  show Add = "+"
+  show Subtract = "-"
+  show Or = "or"
+  show And = "and"
+
 
 data Expr =
       Var String
@@ -9,4 +18,10 @@ data Expr =
     | Neg Expr
     | ABinary BinOp Expr Expr
     | Ap Expr Expr
-    deriving (Show)
+
+instance Show Expr where
+  show (Var x) = x
+  show (IntConst i) = show i
+  show (Neg e) = "-" ++ show e
+  show (ABinary o x y) = printf "(%s %s %s)" (show x) (show o) (show y)
+  show (Ap f x) = printf "(%s %s)" (show f) (show x)
