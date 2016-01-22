b88524e17a7a01963ec8c775d00a20031c6989b9
Nadrieril <nadrieril@gmail.com>
2016-01-22 15:43:10 +0100

    Implement built-in operators as syscalls

--- src/Eval.hs
+++ src/Eval.hs
@@ -4,8 +4,17 @@ import Text.Printf (printf)
 
 import IR.AST
 
-data Val = VInt Integer | VBool Bool | VFun Env Name (Expr Name)
-    deriving (Show)
+data Val =
+    VInt Integer
+  | VBool Bool
+  | VFun Env Name (Expr Name)
+  | VSysCall (Val -> Val)
+
+instance Show Val where
+  show (VInt i) = printf "VInt %s" (show i)
+  show (VBool b) = printf "VBool %s" (show b)
+  show VFun{} = printf "VFun"
+  show VSysCall{} = printf "VSysCall"
 
 
 withInt :: (Integer -> Integer -> Integer) -> Val -> Val -> Val
@@ -16,32 +25,37 @@ withBool :: (Bool -> Bool -> Bool) -> Val -> Val -> Val
 withBool f (VBool x) (VBool y) = VBool $ f x y
 withBool _ v _ = error $ printf "Error: attempting to evaluate %s as bool" (show v)
 
-evalOp :: Name -> Val -> Val -> Val
-evalOp (Name "*") = withInt (*)
-evalOp (Name "/") = withInt div
-evalOp (Name "+") = withInt (+)
-evalOp (Name "-") = withInt (-)
-evalOp (Name "or") = withBool (||)
-evalOp (Name "and") = withBool (&&)
-evalOp (Name "==") = f
-    where
-      f (VInt x) (VInt y) = VBool (x==y)
-      f (VBool x) (VBool y) = VBool (x==y)
-      f x y = error $ printf "Error: %s and %s cannot be compared" (show x) (show y)
-evalOp (Name o) = error $ printf "Error: unknown operator %s" (show o)
-
 ap :: Val -> Val -> Val
 ap (VFun env x v) y = evalE (M.insert x y env) v
+ap (VSysCall f) y = f y
 ap v _ = error $ printf "Error: attempting to evaluate %s as function" (show v)
 
 
 type Env = M.Map Name Val
 
+stdLib :: Env
+stdLib = M.fromList $ map f [
+      ("*", withInt (*)),
+      ("/", withInt div),
+      ("+", withInt (+)),
+      ("-", withInt (-)),
+      ("or", withBool (||)),
+      ("and", withBool (&&)),
+      ("==", eq)
+    ]
+    where
+      eq (VInt x) (VInt y) = VBool (x==y)
+      eq (VBool x) (VBool y) = VBool (x==y)
+      eq x y = error $ printf "Error: %s and %s cannot be compared" (show x) (show y)
+      f (op, fop) = (Name op, VSysCall $ \v1 -> (VSysCall $ \v2 -> fop v1 v2))
+
+
 evalE :: Env -> Expr Name -> Val
 evalE env (Var x) = env M.! x
 evalE _ (Const (B b)) = VBool b
 evalE _ (Const (I i)) = VInt i
-evalE env (Infix o x y) = evalOp o (evalE env x) (evalE env y)
+evalE env (Infix o x y) = evalE env $ Ap (Ap (Var o) x) y
+
 evalE env (Let x v e) = evalE (M.insert x (evalE env v) env) e
 evalE env (LetR f v e) = evalE (M.insert f body env) e
     where body = evalE (M.insert f body env) v
@@ -55,4 +69,4 @@ evalE env (Ap f x) = ap (evalE env f) (evalE env x)
 
 
 eval :: Expr Name -> Val
-eval = evalE M.empty
+eval = evalE stdLib
