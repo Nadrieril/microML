db4a93f219362e4ed384fe22946e4b0c70e5a129
Nadrieril <nadrieril@gmail.com>
2016-02-12 02:26:12 +0100

    Split type reification and function wrapping in StdLib

--- src/Common/StdLib.hs
+++ src/Common/StdLib.hs
@@ -8,7 +8,6 @@ module Common.StdLib
 where
 
 import Data.Proxy (Proxy(..))
-import Text.Printf (printf)
 
 import Common.Expr
 import Common.Type (TConst(..), Mono(..), Poly(..), MonoType, Type)
@@ -29,29 +28,33 @@ getSysCall (Name x) = case x of
 data StdLibValue = Fun (Value -> StdLibValue) | Val Value
 
 
-class StdWrappable a where
+class TypeWrappable a where
     toType :: Proxy a -> MonoType
-    toValue :: a -> StdLibValue
+
+instance TypeWrappable Integer where
+    toType _ = TConst TInt
+instance TypeWrappable Bool where
+    toType _ = TConst TBool
+instance (TypeWrappable a, TypeWrappable b) => TypeWrappable (a -> b) where
+    toType _ = toType (Proxy :: Proxy a) :-> toType (Proxy :: Proxy b)
+
+
+class StdWrappable a where
+    toStdValue :: a -> StdLibValue
 
 instance StdWrappable Value where
-    toType _ = undefined
-    toValue = Val
+    toStdValue = Val
 instance StdWrappable Integer where
-    toType _ = TConst TInt
-    toValue = Val . I
+    toStdValue = Val . I
 instance StdWrappable Bool where
-    toType _ = TConst TBool
-    toValue = Val . B
+    toStdValue = Val . B
 
 instance StdWrappable a => StdWrappable (Value -> a) where
-    toType _ = undefined
-    toValue = Fun . (toValue .)
+    toStdValue = Fun . (toStdValue .)
 instance StdWrappable a => StdWrappable (Integer -> a) where
-    toType _ = TConst TInt :-> toType (Proxy :: Proxy a)
-    toValue f = Fun $ \(I x) -> toValue (f x)
+    toStdValue f = Fun $ \(I x) -> toStdValue (f x)
 instance StdWrappable a => StdWrappable (Bool -> a) where
-    toType _ = TConst TBool :-> toType (Proxy :: Proxy a)
-    toValue f = Fun $ \(B x) -> toValue (f x)
+    toStdValue f = Fun $ \(B x) -> toStdValue (f x)
 
 
 sysCallToValue :: SysCall -> StdLibValue
@@ -68,13 +71,10 @@ sysCallToValType sc = case sc of
         Div -> wrap (div :: Integer -> Integer -> Integer)
         And -> wrap (||)
         Or -> wrap (&&)
-        Eq -> (toValue eq, Bound 0 (Mono (TVar 0 :-> TVar 0 :-> TConst TBool)))
+        Eq -> ( toStdValue ((==) :: Value -> Value -> Bool)
+              , Bound 0 (Mono (TVar 0 :-> TVar 0 :-> TConst TBool)))
     where
         castProxy :: a -> Proxy a
         castProxy = const Proxy
-        wrap :: StdWrappable a => a -> (StdLibValue, Type)
-        wrap x = (toValue x, Mono . toType $ castProxy x)
-
-        eq (I x) (I y) = B (x==y)
-        eq (B x) (B y) = B (x==y)
-        eq x y = error $ printf "Error: %s and %s cannot be compared" (show x) (show y)
+        wrap :: (StdWrappable a, TypeWrappable a) => a -> (StdLibValue, Type)
+        wrap x = (toStdValue x, Mono . toType $ castProxy x)
