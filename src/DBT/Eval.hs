e226fde6d7d77682d32f472363c42221ade43b34
Nadrieril <nadrieril@gmail.com>
2016-02-19 22:21:21 +0100

    Merge products and constructors in DBT evaluation

--- src/DBT/Eval.hs
+++ src/DBT/Eval.hs
@@ -10,7 +10,7 @@ import Data.List (elemIndex)
 import Data.Maybe (fromJust)
 import qualified Data.Map as M
 import Text.Printf (printf)
-import Control.Monad (foldM)
+import Data.Foldable (foldrM)
 import Control.Monad.State (State, evalState, get, put)
 import qualified Debug.Trace as T
 
@@ -26,20 +26,20 @@ type Env e = [Val e]
 
 data Val e =
     Val Value
-  | VProduct Name [Val e]
   | VFun (Env e) e
   | VSysCall (Val e -> Eval e)
-  | VConstructor Name Int [Val e]
+  | VConstructor (ADT Name) Int Int [Val e]
   | VDeconstructor (ADT Name) Int [Val e]
 
 instance Show e => Show (Val e) where
-  show (Val x) = printf "Val %s" (show x)
-  show (VProduct n [x, y]) | isOperator n = printf "(%s %s %s)" (show x) (show n) (show y)
-  show (VProduct n l) = printf "%s%s" (show n) (show l)
-  show (VFun _ e) = printf "VFun(\\%s)" (show e)
-  show VSysCall{} = printf "VSysCall"
-  show VConstructor{} = printf "VConstructor"
-  show VDeconstructor{} = printf "VDeconstructor"
+    show (Val x) = printf "Val %s" (show x)
+    show (VFun _ e) = printf "VFun(\\%s)" (show e)
+    show VSysCall{} = printf "VSysCall"
+    show (VConstructor adt n _ l) = let name = constructorName (adtConstructors adt !! n) in
+        case l of
+            [x, y] | isOperator name -> printf "(%s %s %s)" (show x) (show name) (show y)
+            l -> printf "%s%s" (show name) (show l)
+    show (VDeconstructor adt _ _) = printf "%s.." (show $ deconstructorName adt)
 
 type Eval e = State (Env e) (Val e)
 
@@ -49,10 +49,9 @@ evalSysCall x | x `M.member` adtMap =
         let adt = fst $ adtMap M.! x in
         if x == deconstructorName adt
             then VDeconstructor adt (length $ adtConstructors adt) []
-            else let Constructor n p = constructorsMap adt M.! x in
-                if null p
-                    then VProduct n []
-                    else VConstructor n (length p) []
+            else let n = fromJust (x `elemIndex` map constructorName (adtConstructors adt)) in
+                let Constructor _ p = adtConstructors adt !! n in
+                VConstructor adt n (length p) []
 
 evalSysCall x = aux $ StdLib.sysCallToValue $ StdLib.getSysCall x
     where
@@ -66,17 +65,15 @@ evalAp (VFun stk e) y =
         push y
         evalE e
 evalAp (VSysCall f) y = f y
-evalAp (VConstructor n i l) x = return $
-    if i == 1
-        then VProduct n (reverse $ x:l)
-        else VConstructor n (i-1) (x:l)
-evalAp (VDeconstructor adt i p) x =
-    if i == 0
-        then do
-            let VProduct n l = x
-            let f = reverse p !! fromJust (n `elemIndex` map constructorName (adtConstructors adt))
-            foldM evalAp f l
-        else return $ VDeconstructor adt (i-1) (x:p)
+evalAp (VConstructor _ _ 0 _) _ = error "Attempting to evaluate product as function"
+evalAp (VConstructor adt n i l) x = return $ VConstructor adt n (i-1) (x:l)
+evalAp (VDeconstructor (adtName -> n) 0 _) (VConstructor (adtName -> n') _ 0 _)
+        | n /= n' = error $ printf "Attempting to deconstruct %s value as a %s" (show n) (show n')
+evalAp (VDeconstructor _ 0 p) (VConstructor _ n 0 l) = do
+        let f = p !! (length p - 1 - n)
+        foldrM (flip evalAp) f l
+evalAp (VDeconstructor _ 0 _) _ = error "Attempting to deconstruct non-product value"
+evalAp (VDeconstructor adt i p) x = return $ VDeconstructor adt (i-1) (x:p)
 evalAp v _ = error $ printf "Error: attempting to evaluate %s as function" (show v)
 
 evalE :: Expr -> Eval Expr
